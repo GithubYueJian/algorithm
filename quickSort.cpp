da2534624ba8992db3bec0e937f041ab5fc99e83
Jian Yue <56901351+GithubYueJian@users.noreply.github.com>
2021-08-06 12:33:47 +0800

    Create quickSort.cpp

---
+++ quickSort.cpp
@@ -0,0 +1,64 @@
+/***************************************************************
+Program or Function Name: quick sort
+
+Last Revised: 06/08/2021
+Filename: quickSort.cpp
+Programmer: Jian Yue
+Description: Quick sort
+
+***************************************************************/
+#include<iostream>
+#include<string>
+using std::string;
+#include<vector>
+using std::vector;
+using namespace std;
+
+void quicksort(int left, int right, vector<int> &arr)
+{
+	if (left >= right)
+		return;
+	int i = left;
+	int j = right;
+	int temp = 0;
+	int base = arr[left];
+	while (i < j)
+	{
+		while (i<j&&arr[j]>=base)
+			j--;
+		while (i < j&&arr[i] <= base)
+			i++;
+		if (i < j)
+		{
+			temp = arr[i];
+			arr[i] = arr[j];
+			arr[j] = temp;
+
+		}
+
+	}
+	arr[left] = arr[i];
+	arr[i] = base;
+	quicksort(left, i - 1, arr);
+	quicksort(i + i, right, arr);
+
+}
+int main(void)
+{
+	vector<int> arr{ 5,3,70,-12,-9,100,8,-3,50,300,6 };
+	cout << "Org: " << std::endl;
+	for (auto c : arr)
+	{
+		cout << c << " ";
+	}
+	cout << std::endl;
+	cout << "Sorted: " << std::endl;
+	quicksort(0, 10, arr);
+	for (auto c : arr)
+	{
+		cout << c << " ";
+	}
+	cout << std::endl;
+
+	return 0;
+}
