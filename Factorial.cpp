5de27b0fc067df74aad09569b8e1b6b37a2c6b12
Jian Yue <56901351+GithubYueJian@users.noreply.github.com>
2021-08-18 19:35:22 +0800

    Create Factorial.cpp

---
+++ Factorial.cpp
@@ -0,0 +1,15 @@
+/*
+* Purpose: get n!;
+* Author: Jian Yue
+* Data:   2020/08/18
+*/
+
+#include <iostream>
+using namespace std;
+int Factorial(int n)
+{
+	if (n == 0)
+		return 1;
+	else
+		return n * Factorial(n - 1);
+}
