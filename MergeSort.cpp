7cca3c68beab1b669d15c13fe9d0cce27f4aacff
Jian Yue <56901351+GithubYueJian@users.noreply.github.com>
2021-10-04 17:56:37 +0800

    Update MergeSort.cpp

--- MergeSort.cpp
+++ MergeSort.cpp
@@ -1,7 +1,7 @@
 /*
 * Purpose: merge sorting
 * Author: Jian Yue
-* Date:   2020/08/19
+* Date:   2020/10/04 revised
 */
 #include<iostream>
 #include<string>
@@ -10,68 +10,47 @@ using std::string;
 using std::vector;
 using namespace std;
 
-void merge(vector<int> &arr, int L, int M, int R)
+void merge(vector<int> &arr,int p,int q,int r)
 {
-	int left_size = M - L;
-	int right_size = R - M + 1;
-	vector<int> left_arr(left_size);
-	vector<int> right_arr(right_size);
-	int i, j, k;
-	//fill the left array
-	for (int i = L; i < M; i++)
+	int n1 = q - p + 1;
+	int n2 = r - q;
+	vector<int>L(n1+1);
+	vector<int>R(n2+1);
+	for (int i = 0; i < n1; i++)
+		L[i] = arr[p + i];
+	for (int i = 0; i < n2; i++)
+		R[i] = arr[q + i + 1];
+	L[n1] = INT16_MAX;
+	R[n2] = INT16_MAX;
+	int i = 0, j = 0;
+	for (int k = p; k < r + 1; k++)
 	{
-		left_arr[i - L] = arr[i];
-	}
-
-	//fill the right array
-	for (int i = M; i <= R; i++)
-	{
-		right_arr[i - M] = arr[i];
-	}
-
-	//merge into the original arr
-	i = 0, j = 0, k = L;
-	while (i < left_size && j < right_size)
-	{
-		if (left_arr[i] < right_arr[j])
+		if (L[i] <= R[j])
 		{
-			arr[k] = left_arr[i];
+			arr[k] = L[i];
 			i++;
-			k++;
 		}
 		else
 		{
-			arr[k] = right_arr[j];
+			arr[k] = R[j];
 			j++;
-			k++;
 		}
-
-	}
-	while (i < left_size)
-	{
-		arr[k] = left_arr[i];
-		i++;
-		k++;
-	}
-	while (j < right_size)
-	{
-		arr[k] = right_arr[j];
-		j++;
-		k++;
 	}
 }
-void MergeSort(vector<int> &arr, int L, int R)
+void merge_sort(vector<int> &arr, int p, int r)
 {
-	if (L == R)
-	{
+	if (p == r)
 		return;
-	}
-	else
-	{
-		int M = (L + R) / 2;
-		MergeSort(arr, L, M);
-		MergeSort(arr, M + 1, R);
-		merge(arr, L, M + 1, R);
-	}
+	int q = (p + r) / 2;
+	merge_sort(arr, p, q);
+	merge_sort(arr, q + 1, r);
+	merge(arr, p, q, r);
+}
 
+int main()
+{
+	vector<int> arr {5, 2, 4, 7, 1, 3, 2, 6};
+	merge_sort(arr, 0, 7);
+
+	return 0;
 }
